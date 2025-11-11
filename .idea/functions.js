12ab333431f107e1229a294029770a14a4771d33
Ferran <ferran.garcia@institutvidreres.cat>
2025-11-11 12:58:17 +0100

    Ex2

--- .idea/functions.js
+++ .idea/functions.js
@@ -1,7 +1,17 @@
-function nomicog() {
+// EXERCICI 1
+/* function nomicog() {
     console.log("Ferran")
     console.log("Garcia")
     console.log("Pagès")
 }
 
-nomicog()
+nomicog() */
+
+// EXERCICI 2
+function mostrarTipus(valor) {
+    console.log(typeof valor);
+}
+
+mostrarTipus(42);
+mostrarTipus("Hola");
+mostrarTipus(true);
